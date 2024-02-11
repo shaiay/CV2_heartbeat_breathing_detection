657f2dd14abe187998fae6ffd5f7b49efae7a7de
Ayal Org <atal.org@gmail.com>
2024-02-11 20:10:08 +0200

    simple working

--- frame_processing_logic.py
+++ frame_processing_logic.py
@@ -1,7 +1,5 @@
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-import time
+import numpy as np
 
 
 class FaceROI:
@@ -31,30 +29,6 @@ class FaceROI:
             return [0, 0, 0, 0]
 
 
-cv2.namedWindow("tracking")
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-cap.set(cv2.CAP_PROP_FPS, 10)
-fps = cap.get(cv2.CAP_PROP_FPS)
-print(fps)
-step = int(1000 / fps)
-
-ok, frame = cap.read()
-if not ok:
-    print('Failed to read video from camera')
-    exit()
-
-plt.close()
-fig = plt.figure()
-plt.grid(True)
-plt.ion()  # interactive mode on
-plt.xlabel('Time'),
-plt.ylabel('Amplitude')
-
-plt.title('Heart rate and breath detection')
 DELTA_FRAME_TO_UPDATE_FACE_ROI = 15
 INDEX_TO_START_ACCUMULATING_DATA = 50
 
@@ -71,7 +45,7 @@ class Logic:
         if self.previous_frame is None:
             self._update_bbox_from_frame(frame)
         else:
-            df = (frame - self.previous_frame).abs().mean()
+            df = np.abs(frame - self.previous_frame).mean()
             if df > DELTA_FRAME_TO_UPDATE_FACE_ROI:
                 self._update_bbox_from_frame(frame)
 
@@ -79,7 +53,7 @@ class Logic:
         self.frame_index += 1
 
         if self.frame_index > INDEX_TO_START_ACCUMULATING_DATA:
-            roi = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            roi = frame[self.bbox[1]:self.bbox[3], self.bbox[0]:self.bbox[2]]
             self.green_avg.append(roi[:, :, 1].mean())
 
     def render_face_bbox_to_frame(self, frame):
@@ -89,56 +63,3 @@ class Logic:
         roi = self.face_roi.get_face_ROI(frame)
         if roi[3] > 0:
             self.bbox = roi
-
-
-flag = True
-while (flag):
-    # start = time.clock()
-    ret, frame = cap.read()
-
-    if (idf == 0):
-
-        droi = face_roi.get_face_ROI(frame)
-        if (droi[3] > 0):
-            bbox = droi
-
-    if (idf > 0):
-        df = cv2.absdiff(frame, previous_frame)
-        m_diff = 1.0 * df.sum() / (df.shape[0] * df.shape[1])
-
-        if (m_diff > 15):
-            droi = face_roi.get_face_ROI(frame)
-            if (droi[3] > 0):
-                bbox = droi
-
-    roi = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]];
-    frame = cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-
-    green = getColorAverage(roi, 1)  ## 2nd channel for Green color
-    if (idf > 50):
-        gsums.append(green)
-
-    idf += 1
-    previous_frame = frame
-    cv2.imshow("tracking", frame);
-
-    if (idf > 0):
-        plt.clf()
-        plt.plot(gsums[-200:-1], 'g')
-        plt.pause(0.001)
-        plt.ioff()
-    k = cv2.waitKey(step) & 0xff
-    # end = time.clock()
-    if k == 27:  # esc pressed
-        flag = False
-        break
-
-        # exit when tracking windows is closed
-    if cv2.getWindowProperty('tracking', cv2.WND_PROP_AUTOSIZE) < 1:
-        cap.release()
-        break
-
-plt.close()
-cv2.destroyAllWindows()
-cv2.waitKey(1)
-print("Execution has finished")
--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import PySimpleGUI as sg
-import matplotlib.pyplot as plt
-
+from PIL import Image, ImageTk, ImageOps
 import cv2
+from scipy import signal
 
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg
 from frame_processing_logic import Logic
 
 
-def create_layout():
-    layout = [
-        [sg.Text("Enter data for both graphs:"), sg.InputText(key="data")],
-        [
-            sg.Canvas(key="face"),
-            sg.Canvas(key="green_avg"),
-        ],
-        [sg.Button("Plot Graphs")],
-    ]
-    return layout
+
+def draw_figure(figure):
+    figure_canvas_agg = FigureCanvasAgg(figure)
+    figure_canvas_agg.draw()
+    return ImageTk.PhotoImage(Image.frombuffer('RGBA', (300, 300),figure_canvas_agg.buffer_rgba()))
 
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-update_interval_msec = 100
-layout = create_layout()
+update_interval_msec = 10
+layout = [
+    [
+        sg.Text(key="text")
+    ],
+    [
+        sg.Canvas(key="green_avg", size=(300, 300)),
+        sg.Canvas(key="spectrum", size=(300, 300)),
+    ],
+    [
+        sg.Canvas(key="face", size=(cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))),
+    ],
+]
 
 window = sg.Window("2 Figures and Text Box", layout)
+fig = matplotlib.figure.Figure(figsize=(3, 3), dpi=100)
 logic = Logic()
 
 while True:
@@ -37,6 +46,29 @@ while True:
         break
 
     logic.process_new_frame(frame)
-    window['face'].update(logic.render_face_bbox_to_frame(frame))
+
+    window['text'].update(f"Samples: {len(logic.green_avg)}")
+    frame_with_bbox = ImageTk.PhotoImage(
+        ImageOps.mirror(Image.fromarray(logic.render_face_bbox_to_frame(frame)))
+    )
+    window['face'].TKCanvas.create_image(
+        frame_with_bbox.width() / 2, frame_with_bbox.height() / 2, image=frame_with_bbox
+    )
+
+    fig.clf()
+    ax = fig.add_subplot(111)
+    ax.plot(logic.green_avg[-200:-1], 'g')
+    figure_avg = draw_figure(fig)
+    window['green_avg'].TKCanvas.create_image(150, 150, image=figure_avg)
+
+    if len(logic.green_avg) % 8 == 0:
+        fig.clf()
+        ax = fig.add_subplot(111)
+        f, p = signal.welch(logic.green_avg, fs=1000 / update_interval_msec)
+        ax.plot(f, p, 'r')
+        figure_spectrum = draw_figure(fig)
+        window['spectrum'].TKCanvas.create_image(150, 150, image=figure_spectrum)
+
+
 
 window.close()
